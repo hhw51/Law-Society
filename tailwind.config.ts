7b1da57c71b5d8c26c241d7ac670056cc966233f
hhw51 <harishassanwyne@gmail.com>
2026-01-05 04:59:21 +0500

    t1 v3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
